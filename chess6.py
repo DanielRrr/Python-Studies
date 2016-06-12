d748c3365274c51f227283ed77d49e47dfa840e9
Danile Rogozin <chelsea2005joecole@icloud.com>
2016-06-12 22:52:16 +0300

    Create chess6.py

---
+++ chess6.py
@@ -0,0 +1,22 @@
+a = int(input())
+b = int(input())
+c = int(input())
+d = int(input())
+if c == a - 1 and d == b - 2:
+    print("YES")
+elif c == a + 1 and d == b - 2:
+    print("YES")
+elif c == a - 2 and d == b - 1:
+    print("YES")
+elif c == a + 2 and d == b - 1:
+    print("YES")
+elif c == a - 1 and d == b + 2:
+    print("YES")
+elif c == a + 1 and d == b + 2:
+    print("YES")
+elif c == a - 2 and d == b + 1:
+    print("YES")
+elif c == a + 2 and d == b + 1: 
+    print("YES")
+else:
+    print("NO")
