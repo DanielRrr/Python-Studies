9d817e435e42eb7573d00234f90440c02b2ecea4
Danile Rogozin <chelsea2005joecole@icloud.com>
2016-07-17 14:01:35 +0300

    Create webscrap10.py

---
+++ webscrap10.py
@@ -0,0 +1,36 @@
+from urllib.request import urlopen
+from bs4 import BeautifulSoup
+import re
+import string
+import operator
+
+def cleanInput(input):
+    input = re.sub('\n+', " ", input).lower()
+    input = re.sub('\[[0-9]*\]', "", input)
+    input = re.sub(' +', " ", input)
+    input = bytes(input, "UTF-8")
+    input = input.decode("ascii", "ignore")
+    cleanInput = []
+    input = input.split(' ')
+    for item in input:
+        item = item.strip(string.punctuation)
+        if len(item) > 1 or (item.lower() == 'a' or item.lower() == 'i'):
+            cleanInput.append(item)
+    return cleanInput
+
+def ngrams(input, n):
+    input = cleanInput(input)
+    output = {}
+    for i in range(len(input)-n+1):
+        ngramTemp = " ".join(input[i:i+n])
+        if ngramTemp not in output:
+            output[ngramTemp] = 0
+        output[ngramTemp] += 1
+    return output
+
+url = str(input())
+k = int(input())
+content = str(urlopen(url).read(), 'utf-8')
+ngrams = ngrams(content, k)
+sortedNGrams = sorted(ngrams.items(), key = operator.itemgetter(1), reverse = True)
+print(sortedNGrams)
